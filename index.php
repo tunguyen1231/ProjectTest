5f844df9eafd984a1f6c0cde00655113fe6f6969
tunguyen1231 <tunguyen01687172821@gmail.com>
2019-05-21 19:26:09 +0700

    .

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
-<?php
-	phpinfo();
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-sm-12">
+	            <h1>Tính chỉ số BMI</h1>
 
-?>
+	            <form name="bmi" action="" method="post">
+	                <div class="form-group">
+	                    <label >Chiều cao ( cm )</label>
+	                    <input type="text" class="form-control" name="height" value="">
+	                </div>
+	                <div class="form-group">
+	                    <label >Cân nặng ( kg ) </label>
+	                    <input type="text" class="form-control" name="weight">
+	                </div>
+	                <button type="submit" name="calc" class="btn btn-primary">Submit</button>
+	            </form>
+	        </div>
+	    </div>
+	</div>
+</body>
+</html>
