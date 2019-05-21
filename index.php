2837f68f866e6227cb595f53b8eaa1bf85d1fda1
tunguyen1231 <tunguyen01687172821@gmail.com>
2019-05-21 20:09:17 +0700

    .

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 
 		if ($bmi < 18.5) {
-            echo "Phân loại : Dưới chuẩn";
+            echo "Phân loại : Đcm que củi ư )))";
         } elseif ( ($bmi >= 18.5) && ($bmi <= 24.9 ) ) {
             echo "Phân loại : Chuẩn rồi";
         } elseif ( ($bmi >= 25) && ($bmi <= 29.9 )) {
